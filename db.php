ecda763360176fd3dd8d4330c0599a1176debe3b
Richardhnb7 <2877987@alu.murciaeduca.es>
2021-10-20 21:12:15 +0200

    cambios

---
+++ db.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title></title>
+</head>
+
+<body> 
+	<?php
+		
+		echo"hola mundo";
+	
+	?>
+</body>
+</html>
+<?php
+		
+	echo"hola mundo";
+	
+?>
---
+++ dwes/ejercicio2.php
@@ -0,0 +1,10 @@
+<?php
+	$var1= 100;
+	$var2= &$var1;
+	$var3= $var1;
+	echo "$var2<br>";
+	$var2 =300;
+	echo "$var1<br>";
+	$var3 = 400;
+	echo $var1;
+?>
